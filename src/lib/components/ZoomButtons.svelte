aa99d1cb8fb9bff3341aebeb8dcff0961e2f6100
Johannes Pertl <johannes.pertl@proton.me>
2023-12-14 23:36:55 +0100

    Change zoom to fontSize increase

--- src/lib/components/ZoomButtons.svelte
+++ src/lib/components/ZoomButtons.svelte
@@ -1,26 +1,36 @@
 <script lang="ts">
 	let zoomLevel = 1;
-	const maxZoomLevel = 2;
+	const maxZoomLevel = 1.4;
 	const minZoomLevel = 1;
 
 	function zoomIn() {
 		if (zoomLevel < maxZoomLevel) {
-			zoomLevel += 0.2;
-			(document.body.style as any).zoom = zoomLevel;
+			zoomLevel += 0.1;
+			document.documentElement.style.fontSize = zoomLevel + 'em';
 		}
 	}
 
 	function zoomOut() {
 		if (zoomLevel > minZoomLevel) {
-			zoomLevel -= 0.2;
-			(document.body.style as any).zoom = zoomLevel;
+			zoomLevel -= 0.1;
+			document.documentElement.style.fontSize = zoomLevel + 'em';
 		}
 	}
 </script>
 
-<button class="btn btn-icon-sm hidden sm:flex variant-filled" on:click={zoomIn} title="Schrift größer" aria-label="Hineinzoomen">
-    <span class="material-symbols-outlined">add</span>
+<button
+	class="btn btn-icon-sm hidden sm:flex variant-filled"
+	on:click={zoomIn}
+	title="Schrift größer"
+	aria-label="Hineinzoomen"
+>
+	<span class="material-symbols-outlined">add</span>
 </button>
-<button class="btn btn-icon-sm hidden sm:flex variant-filled" on:click={zoomOut} title="Schrift kleiner" aria-label="Herauszoomen">
-    <span class="material-symbols-outlined">remove</span>
+<button
+	class="btn btn-icon-sm hidden sm:flex variant-filled"
+	on:click={zoomOut}
+	title="Schrift kleiner"
+	aria-label="Herauszoomen"
+>
+	<span class="material-symbols-outlined">remove</span>
 </button>
