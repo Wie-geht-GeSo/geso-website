ae300a0674ec3d8199192fb4ad6429432c4a303f
Johannes Pertl <johannes.pertl@proton.me>
2023-12-17 18:28:38 +0100

    Change font to Satoshi

--- geso-theme.ts
+++ geso-theme.ts
@@ -4,8 +4,8 @@ export const gesoTheme: CustomThemeConfig = {
 	name: 'geso',
 	properties: {
 		// =~= Theme Properties =~=
-		"--theme-font-family-base": `system-ui`,
-		"--theme-font-family-heading": `system-ui`,
+        "--theme-font-family-base": `'Satoshi', sans-serif`,
+        "--theme-font-family-heading": `'Satoshi', sans-serif`,
 		"--theme-font-color-base": "0 0 0",
 		"--theme-font-color-dark": "255 255 255",
 		"--theme-rounded-base": "9999px",
