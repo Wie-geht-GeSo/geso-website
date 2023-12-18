f8af0afda52045229798eb3123af127ef153abdb
Johannes Pertl <johannes.pertl@proton.me>
2023-12-18 23:51:47 +0100

    Increase gap for footer blocks

--- src/lib/components/Footer.svelte
+++ src/lib/components/Footer.svelte
@@ -24,7 +24,7 @@
 				<p class="!text-sm text-center md:text-start">{$page.data.globals.websiteSlogan}</p>
 				<p class="!text-sm mt-2 text-center md:text-start dynamic-html">{@html $page.data.footer.content || ""}</p>
 			</div>
-			<div class="grid grid-cols-3 md:gap-8 text-center md:text-start">
+			<div class="grid grid-cols-3 md:gap-20 text-center md:text-start">
 				{#each $page.data.footer.blocks as block (block.id)}
 					{#if block.collection === 'blockLinkGroup'}
 						<div class="space-y-6">
