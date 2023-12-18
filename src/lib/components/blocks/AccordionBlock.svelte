72138af9605ce6d712f90dd9a37a9be4d1c6cc74
Johannes Pertl <johannes.pertl@proton.me>
2023-12-18 23:39:42 +0100

    Add default icon for Accordion

--- src/lib/components/blocks/AccordionBlock.svelte
+++ src/lib/components/blocks/AccordionBlock.svelte
@@ -1,23 +1,24 @@
 <script lang="ts">
-    import type { AccordionBlock } from '$lib/types/blocks/AccordionBlock';
+	import type { AccordionBlock } from '$lib/types/blocks/AccordionBlock';
 	import { Accordion, AccordionItem } from '@skeletonlabs/skeleton';
 
-    export let data: AccordionBlock;
+	export let data: AccordionBlock;
 </script>
 
-
 <div class="py-3">
-    <Accordion class="card">
-        {#each data.items as accordionItemBlock}
-            <AccordionItem class="h-full">
-                <svelte:fragment slot="lead">
-                    <span class="material-symbols-outlined">{accordionItemBlock.icon}</span>
-                </svelte:fragment>
-                <svelte:fragment slot="summary">{accordionItemBlock.title}</svelte:fragment>
-                <svelte:fragment slot="content">
-                    <div class="dynamic-html">{@html accordionItemBlock.content}<div/>
-                </svelte:fragment>
-            </AccordionItem>
-        {/each}
-    </Accordion>
+	<Accordion class="card" spacing="space-y-5" >
+		{#each data.items as accordionItemBlock}
+			<AccordionItem class="h-full">
+				<svelte:fragment slot="lead">
+					<span class="material-symbols-outlined">{accordionItemBlock.icon ? accordionItemBlock.icon : "question_mark"}</span>
+				</svelte:fragment>
+				<svelte:fragment slot="summary">{accordionItemBlock.title}</svelte:fragment>
+				<svelte:fragment slot="content">
+					<div class="dynamic-html">
+						{@html accordionItemBlock.content}
+					</div>
+                    </svelte:fragment>
+			</AccordionItem>
+		{/each}
+	</Accordion>
 </div>
